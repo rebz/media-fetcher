d1a397831c711d464acd5c526ece2e0afa52e313
Christopher Johnson <esreborn@gmail.com>
2020-07-04 11:15:08 -0400

    revert tmdb api back to v3 and create request interceptor to attach api key to all outgoing requests

--- backend/utils/TMDB.js
+++ backend/utils/TMDB.js
@@ -3,10 +3,20 @@ import { TMDB_API_ENDPOINT, TMDB_API_TOKEN } from './../config/tmdb'
 
 const http = axios.create({
     baseURL: TMDB_API_ENDPOINT,
-    headers: {
-        Authorization: `Bearer ${TMDB_API_TOKEN}`
-    }
+    // only used w/ v4 of the api, we are back to v3
+    // headers: { 
+    //     Authorization: `Bearer ${TMDB_API_TOKEN}`
+    // }
 })
+
+// attach api key on every outgoing request
+const apiKeyInterceptor = http.interceptors.request.use(function (config) {
+    // if params is not previously defined, create its
+    if (!('params' in config)) config['params'] = {}
+    config.params['api_key'] = TMDB_API_TOKEN
+    return config
+});
+// http.interceptors.request.eject(apiKeyInterceptor);
 
 const TMDB = {
 
@@ -22,6 +32,26 @@ const TMDB = {
             throw error
         }
     },
+
+    getMovie: async (id) => {
+        if (!id) throw '`ids` not defined for getMovie(id)'
+        try {
+            const response = await http.get(`movie/${id}`)
+            return response.data
+        } catch (error) {
+            throw error
+        }
+    },
+
+    getTv: async (id) => {
+        try {
+            const response = await http.get(`tv/${id}`)
+            return response.data
+        } catch (error) {
+            throw error
+        }
+    },
+
 }
 
 export { 
